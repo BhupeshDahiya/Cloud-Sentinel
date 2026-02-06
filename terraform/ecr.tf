ce5574fea9930af06971cbdc395b32bed309b4c3
BhupeshDahiya <bhupesh40rajput@gmail.com>
2026-02-06 22:10:20 +0530

    added sec layer before pushing docker img to ecr, updated ecs.tf to auto delete old images and changed to app to update info in realtime

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -2,3 +2,20 @@ resource "aws_ecr_repository" "app_repo" {
   name         = "${var.app_name}-repo"
   force_delete = true
 }
+
+resource "aws_ecr_lifecycle_policy" "cleanup" {
+  repository = aws_ecr_repository.app_repo.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 5 images"
+      selection = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 5
+      }
+      action = { type = "expire" }
+    }]
+  })
+}
